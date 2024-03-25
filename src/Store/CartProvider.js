b212c1fa74dd8263eaf250e0b7050da06e42f6dc
ankit <ankitiwari26@gmail.com>
2024-03-25 14:48:54 +0530

    Seventh Commit

---
+++ src/Store/CartProvider.js
@@ -0,0 +1,19 @@
+import CartContext from "./cart-context";
+
+const CartProvider = (props) => {
+  const addItemCartHandler = (item) => {};
+  const removeItemFromCartHandler = (id) => {};
+  const cartContext = {
+    item: [],
+    totalAmount: 0,
+    addItem: addItemCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+export default CartProvider;
